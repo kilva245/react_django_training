7488e0dfa434fc81db3afa392e6c23865c148926
kilva245 <139843031+kilva245@users.noreply.github.com>
2025-06-18 05:42:59 +0330

    context summary
    learn about context and how it works

--- Django_learn/djlearn/djlearn/views.py
+++ Django_learn/djlearn/djlearn/views.py
@@ -4,4 +4,12 @@ def homepage(request):
     return render(request=request, template_name='home.html')
 
 def temp(request):
-    return render(request=request, template_name='temp.html')
+    context = {
+        'key' : 'value',
+        'name' : 'John',
+        'person' : {
+            'name' : 'John',
+            'age' : '26'
+        }
+    }
+    return render(request=request, template_name='temp.html', context=context)
